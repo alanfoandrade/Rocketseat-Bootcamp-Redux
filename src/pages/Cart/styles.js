540e19857a864d517e81b23bd8130551697ca254
alanfoandrade <alanfrank@gec.inatel.br>
2019-11-13 19:40:10 -0300

    Add productList on Cart + immer to change amount on state

--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -79,6 +79,12 @@ export const ProductTable = styled.table`
       padding: 6px;
     }
   }
+
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+  }
 `;
 
 export const Total = styled.div`

af73836b6fd11a9a8cb15a97aae10f555e4cf0c0
alanfoandrade <alanfrank@gec.inatel.br>
2019-11-14 18:29:24 -0300

    Add redux-saga + reactotron plugin

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,10 +1,13 @@
 import reactotronReactJs from 'reactotron-react-js';
+
 import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
   const tron = reactotronReactJs
     .configure()
     .use(reactotronRedux())
+    .use(reactotronSaga())
     .connect();
 
   tron.clear();

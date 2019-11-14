fd1d4feb76fe43427cb52e0531d029020c2786de
alanfoandrade <alanfrank@gec.inatel.br>
2019-11-14 20:45:10 -0300

    Add history + navigation to cart on saga

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -3,9 +3,9 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import { formatPriceBrl } from '../../../util/format';
-
 import api from '../../../services/api';
-import { addToCartSuccess, updateAmount } from './actions';
+import history from '../../../services/history';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
   const productExistis = yield select(state =>
@@ -25,7 +25,7 @@ function* addToCart({ id }) {
   }
 
   if (productExistis) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
 
@@ -36,7 +36,26 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+
+    history.push('/cart');
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Estoque insuficiente');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
